fd916c4fa16d67d6fd04c587e1c2bb8c0e319521
Jorin Manandhar <nirose@jucktion.com>
2022-01-24 18:13:06 +0545

    fix for remaining message update

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def set_timer(message):
         if type(int(message.text)) == int:
             TIMER = int(message.text)
             bot.send_message(chat_id=message.chat.id,
-                                    text=f'Timer set to:{remain(TIMER)}',
+                                    text=f'Timer set to: {remain(TIMER)}',
                                     reply_markup=show_options(),
                                     parse_mode='HTML')
     except:
@@ -100,7 +100,7 @@ def handle_command_start(message):
     text = f"Let's start" if (done == 0) else f"It's time:"
     option = 'Start' if (done == 0) else 'Done'
     if done <= 1:
-        print('option id: ',option_messages, len(option_messages))
+        #print('option id: ',option_messages, len(option_messages))
         try:
             delete_old_msg(message.chat.id, option_messages)
         except Exception as e:
@@ -135,11 +135,11 @@ def handle_command_set_url(message):
 @bot.message_handler(func=lambda message: True, content_types=["text"], is_chat_admin=True)
 def handle_command_check(message):
     remaining = end_time-time.time()
-    remaining_string = remain(end_time-time.time()) if (end_time > 0) else 0
+    remaining_string = remain(end_time-time.time()) if (remaining > 0) else str(timedelta(seconds = time.time() - end_time)).split('.')[0]
 
     isitgone = 'remaining' if (remaining > 0) else 'not set' if (end_time == 0) else 'elapsed'
-    print('remaining id: ',remaining_messages, len(remaining_messages))
-    if remaining > 0 and len(remaining_messages) > 0:
+    #print('before remaining id: ',message.message_id, remaining_messages, len(remaining_messages))
+    if remaining > 0 or message.message_id == remaining_messages[0]:
         remaining_message = bot.edit_message_text(chat_id=message.chat.id,
                     reply_markup=check_remaining(),
                     text=f"Time {isitgone}: {remaining_string}",
@@ -151,7 +151,11 @@ def handle_command_check(message):
                     text=f"Time {isitgone}: {remaining_string}",
                     parse_mode='HTML')
     if remaining_message.message_id not in remaining_messages:
-        remaining_messages.append(remaining_message.message_id)
+        if len(remaining_messages) > 0:
+                remaining_messages[0] = remaining_message.message_id
+        else:
+            remaining_messages.append(remaining_message.message_id)
+        #print('after remaining id: ',remaining_messages, len(remaining_messages))
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -160,11 +164,14 @@ def handle_query(call):
     global done
     if (call.data.startswith("['done'")):
         remaining_message = bot.edit_message_text(chat_id=call.message.chat.id,
-                              text=f"Waiting for: {remain(int(TIMER))}",
+                              text=f"Time remaining: {remain(int(TIMER))}",
                               reply_markup=check_remaining(),
                               message_id=call.message.message_id)
         if remaining_message.message_id not in remaining_messages:
-            remaining_messages.append(remaining_message.message_id)
+            if len(remaining_messages) > 0:
+                remaining_messages[0] = remaining_message.message_id
+            else:
+                remaining_messages.append(remaining_message.message_id)
         end_time = get_end_time()
         time.sleep(int(TIMER))
         done = done + 1
